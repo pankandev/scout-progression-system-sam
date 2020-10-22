3178f7b3b9f85cd63305641c56d432f840742cbe
Patricio Campaña <pcampaa@gmail.com>
2020-10-22 19:05:48 -0300

    fix exception handling

--- pps/groups/app.py
+++ pps/groups/app.py
@@ -99,7 +99,7 @@ class BeneficiariesService(ModelService):
         try:
             interface.create(join_key(district, group, unit), beneficiary, code, raise_if_exists=True)
             return True
-        except interface.client.exceptions.ConditionalCheckFailedException:
+        except interface.client.meta.client.exceptions.ConditionalCheckFailedException:
             return False
 
 
