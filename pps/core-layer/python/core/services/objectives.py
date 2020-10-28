73cbe29e13bebcafb994577d27e7a1ba03fa9258
Patricio Campaña <pcampaa@gmail.com>
2020-10-28 20:05:33 -0300

    update objectives table key schema

--- pps/core-layer/python/core/services/objectives.py
+++ pps/core-layer/python/core/services/objectives.py
@@ -4,18 +4,18 @@ from core.utils import join_key
 
 class ObjectivesService(ModelService):
     __table_name__ = "objectives"
-    __partition_key__ = "unit-stage"
-    __sort_key__ = "code"
+    __partition_key__ = "area"
+    __sort_key__ = "subline"
 
-    def create(self, unit: str, stage: str, area: str, line: int, description: str):
+    def create(self, unit: str, stage: str, area: str, line: int, sub_line: int, description: str):
         pass
 
     @classmethod
-    def get(cls, unit: str, stage: str, area: str, line: int):
+    def get(cls, stage: str, area: str, line: int, sub_line: int):
         interface = cls.get_interface()
-        return interface.get(join_key(unit, stage), join_key(area, line), attributes=['description'])
+        return interface.get(stage, join_key(area, line, sub_line), attributes=['description'])
 
     @classmethod
-    def query(cls, unit: str, stage: str):
+    def query(cls, stage: str):
         interface = cls.get_interface()
-        return interface.query(join_key(unit, stage))
+        return interface.query(stage)
