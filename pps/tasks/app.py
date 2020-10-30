0f1513aa18675940f98bfead60f206131cd35355
Patricio Campaña <pcampaa@gmail.com>
2020-10-30 02:05:41 -0300

    add base tasks function

---
+++ pps/tasks/app.py
@@ -0,0 +1,31 @@
+from core import HTTPEvent, JSONResponse
+from core.aws.errors import HTTPError
+from core.services.tasks import TasksService
+
+
+def get_handler(event: HTTPEvent):
+    if event.resource.split('/')[-1] == 'tasks':
+        # get beneficiary tasks
+        sub = event.params['sub']
+        if event.authorizer.sub != sub:
+            return JSONResponse.generate_error(HTTPError.FORBIDDEN, "You have no access to this resource with this user")
+        return TasksService.query(event.authorizer)
+    return JSONResponse.generate_error(HTTPError.UNKNOWN_RESOURCE, f"Unknown resource {event.resource}")
+
+
+def post_handler(event: HTTPEvent):
+    if event.resource.split('/')[-1] == 'tasks':
+        # create new task
+        sub = event.params['sub']
+        if event.authorizer.sub != sub:
+            return JSONResponse.generate_error(HTTPError.FORBIDDEN, "You have no access to this resource with this user")
+        tasks = TasksService.query(event.authorizer)
+
+    return JSONResponse.generate_error(HTTPError.UNKNOWN_RESOURCE, f"Unknown resource {event.resource}")
+
+
+def handler(event: dict, _) -> dict:
+    event = HTTPEvent(event)
+    if event.method == "GET":
+        response = get_handler(event)
+    return response.as_dict()
