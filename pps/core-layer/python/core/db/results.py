3735f311f79d80d631cf6b33660c654d12f36ef7
Patricio Campaña <pcampaa@gmail.com>
2020-10-30 16:59:29 -0300

    add print

--- pps/core-layer/python/core/db/results.py
+++ pps/core-layer/python/core/db/results.py
@@ -19,6 +19,7 @@ class QueryResult(Result):
         self.consumed_capacity = ConsumedCapacity.from_dict(result.get('ConsumedCapacity'))
 
     def as_dict(self):
+        print(self.count, self.items, self.last_evaluated_key)
         return {
             "items": self.items,
             "count": self.count,
